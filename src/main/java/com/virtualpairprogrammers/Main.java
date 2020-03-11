e9e256a71c63ac718a8a0e85ce1060d8fc8babce
Akshit <akshitgoel.in@gmail.com>
2020-03-11 17:36:32 +0530

    PairRDD

---
+++ src/main/java/com/virtualpairprogrammers/Main.java
@@ -0,0 +1,39 @@
+package com.virtualpairprogrammers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.spark_project.guava.collect.Iterables;
+
+import scala.Tuple2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List<String> inputData = new ArrayList<>();
+		inputData.add("WARN: Tuesday 4 September 0405");
+		inputData.add("ERROR: Tuesday 4 September 0408");
+		inputData.add("FATAL: Wednesday 5 September 1632");
+		inputData.add("ERROR: Friday 7 September 1854");
+		inputData.add("WARN: Saturday 8 September 1942");
+
+		Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+		SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+
+		sc.parallelize(inputData)
+			.mapToPair(rawValue -> new Tuple2<>(rawValue.split(":")[0], 1L))
+			.reduceByKey((value1, value2) -> value1 + value2).collect()
+			.forEach(tuple -> System.out.println(tuple._1 + " has " + tuple._2 + " instances"));
+		
+
+	}
+
+}
